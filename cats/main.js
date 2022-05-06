c9790d6e999df403f2e6d9d6755c051ecee01423
Lukease <lukpawlowski@gmail.com>
2022-05-06 14:35:32 +0200

    jquery

--- cats/main.js
+++ cats/main.js
@@ -1,65 +1,40 @@
-const app = document.querySelector('.app')
+const app = $('.app')
 
 const createArena = () => {
-    const arenaBox = document.createElement('div')
-
-    arenaBox.classList.add('app__cards')
-    app.appendChild(arenaBox)
-
-    const arena = document.createElement('div')
-
-    arena.classList.add('cards-arena')
-    arenaBox.appendChild(arena)
+    const arenaBox = $('<div></div>').addClass('app__cards').appendTo(app)
+    const arena = $('<div></div>').addClass('cards-arena').appendTo(arenaBox)
 
     return arena
 }
 
 let scoreNumber = 0
 const createScoreBox = () => {
-    const scoreBox = document.createElement('div')
-
-    scoreBox.classList.add('app__score')
-    app.appendChild(scoreBox)
-
-    const score = document.createElement('div')
-
-    score.classList.add('score')
-    scoreBox.appendChild(score)
-
-    const scoreSpan = document.createElement('span')
-
-    scoreSpan.classList.add('score__span')
-    score.appendChild(scoreSpan)
-    scoreSpan.innerHTML = 'score: '
-
-    const scoreValue = document.createElement('span')
-
-    scoreValue.classList.add('score__value')
-    score.appendChild(scoreValue)
-    scoreValue.innerHTML = scoreNumber
+    const scoreBox = $('<div></div>').addClass('app__score').appendTo(app)
+    const score = $('<div></div>').addClass('score').appendTo(scoreBox)
+    const scoreSpan = $('<span> score: </span>').addClass('score__span').appendTo(score)
+    const scoreValue = $('<span></span>').addClass('score__value').appendTo(score).text(scoreNumber)
 
     return scoreValue
 }
 
 const click = event => {
     if (clickLimit > 0) {
-        event.target.classList.add('hide')
+        $(event.target).addClass('hide')
         clickLimit--
-        event.target.nextElementSibling.classList.add('show')
-        clickedList = clickedList.concat(event.target.nextElementSibling.src)
-        clickedIndex = clickedIndex.concat(event.target)
+        $(event.target).siblings().addClass('show')
+        clickedList = clickedList.concat($(event.target).siblings().attr('src'))
+        clickedIndex = clickedIndex.concat($(event.target))
     }
 
     const [firstCard, secondCard] = clickedList
 
     if (clickLimit === 0 && firstCard === secondCard) {
         setTimeout(() => {
-            event.target.removeEventListener('click', click)
-
-            const points = document.querySelector('.score__value')
-
+            $(event.target).off('click', click)
             scoreNumber += 1
-            points.innerHTML = scoreNumber
+
+            const points = $('.score__value').text(scoreNumber)
+
             clickLimit = 2
             clickedList = []
             clickedIndex = []
@@ -74,8 +49,8 @@ const click = event => {
 
     if (clickLimit === 0 && firstCard !== secondCard) {
         setTimeout(() => {
-            clickedIndex[0].classList.remove('hide')
-            event.target.classList.remove('hide')
+            clickedIndex[0].removeClass('hide')
+            $(event.target).removeClass('hide')
             clickLimit = 2
             clickedList = []
             clickedIndex = []
@@ -84,24 +59,16 @@ const click = event => {
 }
 
 const createCards = () => {
-    const newCard = document.createElement('div')
+    const newCard = $('<div></div>').addClass('card').appendTo(arena)
+    const cardBack = $('<div></div>').addClass('card__back').appendTo(newCard)
 
-    newCard.classList.add('card')
-    arena.appendChild(newCard)
-
-    const cardBack = document.createElement('div')
-
-    cardBack.classList.add('card__back')
-    newCard.appendChild(cardBack)
-
-    cardBack.addEventListener('click', click)
+    cardBack.on('click',click)
 
     return cardBack
 }
 
 const arena = createArena()
 const scoreBox = createScoreBox()
-const imagesLocation = './cats/'
 let imagesArray = Array.from(new Array(10)).map((object, index) => index)
 let clickedList = []
 let clickLimit = 2
@@ -120,10 +87,6 @@ addHiddenImages()
 sortedArrayOfImages.forEach((object, index) => {
     createCards()
 
-    const newCard = document.querySelectorAll('.card')
-    const image = document.createElement('img')
-
-    image.src = `${imagesLocation}cat${object + 1}.jpg`
-    image.classList.add('card__front')
-    newCard[index].appendChild(image)
+    const newCard = $('.card').eq(index)
+    const image = $('<img>').addClass('card__front').appendTo(newCard).attr('src' ,`./cats/cat${object + 1}.jpg`)
 })
